b6f3ddd8f462845bb32df4b885e2e81bcf66240e
Carlos Patitucci <carlosapatitucci@gmail.com>
2024-01-16 01:51:20 -0300

    segunda entrega

--- desafio-patitucci.js
+++ desafio-patitucci.js
@@ -1,21 +1,21 @@
+const fs = require("fs").promises;
+
 class ProductManager {
-    #id = 1;
+    static ultId = 0;
 
-    constructor() {
+    constructor(path) {
         this.productos = [];
-    }
-
-    getProducts() {
-        return this.productos;
+        this.path = path;
     }
 
     codigoUnico(code) {
         return !this.productos.some(product => product.code === code);
     }
 
-    addProducts(title, description, price, thumbnail, code, stock) {
+    async addProduct(nuevoProducto) {
+        let { title, description, price, img, code, stock } = nuevoProducto;
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
+        if (!title || !description || !price || !img || !code || !stock) {
             console.log('Error: Todos los campos son obligatorios. Asegúrate de proporcionar valores para cada campo.');
             return;
         }
@@ -25,40 +25,197 @@ class ProductManager {
             return;
         }
 
-        let product = {};
-        product.title = title;
-        product.description = description;
-        product.price = price;
-        product.thumbnail = thumbnail;
-        product.code = code;
-        product.stock = stock;
-        product.id = this.#id++;
-        this.productos.push(product);
+        const newProduct = {
+            id: ++ProductManager.ultId,
+            title,
+            description,
+            price,
+            img,
+            code,
+            stock
+        }
+
+
+        this.productos.push(newProduct);
+
+        await this.guardarArchivo(this.productos);
+
     }
 
-    getProductById(id) {
-        const product = this.productos.find(product => product.id === id);
-        return product || null;
+    getproductos() {
+        console.log(this.productos);
     }
+
+    async getProductById(id) {
+        try {
+            const arregloProductos = await this.leerArchivo();
+            const productoBuscado = arregloProductos.find(item => item.id === id);
+
+            if (!productoBuscado) {
+                console.log("Producto no encontrado");
+            } else {
+                console.log("El producto buscado existe");
+                return productoBuscado;
+            }
+
+        } catch (error) {
+            console.log("Error al leer el archivo ", error);
+        }
+
+    }
+
+    async leerArchivo() {
+        try {
+            const respuesta = await fs.readFile(this.path, "utf-8");
+            const arregloProductos = JSON.parse(respuesta);
+            return arregloProductos;
+
+        } catch (error) {
+            console.log("Error al leer un archivo", error);
+        }
+    }
+
+    async guardarArchivo(arregloProductos) {
+        try {
+            await fs.writeFile(this.path, JSON.stringify(arregloProductos, null, 2));
+        } catch (error) {
+            console.log("Error al guardar el archivo", error);
+        }
+    }
+
+    async updateProduct(id, productoActualizado) {
+        try {
+            const arregloProductos = await this.leerArchivo();
+
+            const index = arregloProductos.findIndex(item => item.id === id);
+
+            if (index !== -1) {
+                arregloProductos.splice(index, 1, productoActualizado);
+                await this.guardarArchivo(arregloProductos);
+            } else {
+                console.log("no se encontró el producto");
+            }
+
+        } catch (error) {
+            console.log("Error al actualizar el producto", error);
+        }
+    }
+    async deleteProduct(id) {
+        try {
+            const arregloProductos = await this.leerArchivo();
+
+            const index = arregloProductos.findIndex(item => item.id === id);
+
+            if (index !== -1) {
+                arregloProductos.splice(index, 1);
+                await this.guardarArchivo(arregloProductos);
+                console.log("Producto eliminado exitosamente.");
+            } else {
+                console.log("No se encontró el producto con el ID proporcionado.");
+            }
+        } catch (error) {
+            console.log("Error al eliminar el producto", error);
+        }
+    }
+
 }
 
-const productosCreados = new ProductManager();
-productosCreados.addProducts('Zanahoria', 'vegetal naranja', 100, 'https://img.freepik.com/vector-premium/icono-ilustracion-dibujos-animados-vector-vegetal-zanahoria-fresca_472998-308.jpg?w=2000', 'COD01', 1010);
-productosCreados.addProducts('Manzana', 'fruta roja y jugosa', 250, 'https://img.freepik.com/vector-gratis/ilustracion-icono-dibujos-animados-fruta-manzana-concepto-icono-fruta-alimentos-aislado-estilo-dibujos-animados-plana_138676-2922.jpg?size=338&ext=jpg&ga=GA1.1.1687694167.1704153600&semt=ais', 'COD03', 510);
-productosCreados.addProducts('Banana', 'fruta amarilla y nutritiva', 350, 'https://static.vecteezy.com/system/resources/previews/024/734/340/non_2x/cartoon-illustration-of-two-bananas-vector.jpg', 'COD05', 310);
-productosCreados.addProducts('Lechuga', 'vegetal verde', 150, 'https://img.freepik.com/vector-premium/ilustracion-icono-vector-dibujos-animados-lechuga-mostaza-verde-flotante_480044-617.jpg', 'COD05', 440);
-productosCreados.addProducts('Tomate', 'ideal para una ensalada', 250, '', 'COD07', 540);
 
-console.log(productosCreados.getProducts());
+const manager = new ProductManager("./productos.json");
 
-const productIdBuscar = 3;
-const buscarProducto = productosCreados.getProductById(productIdBuscar);
+manager.getproductos();
 
-if (buscarProducto) {
-    console.log(`Producto encontrado: ${buscarProducto.title} - Precio: $${buscarProducto.price}`);
-} else {
-    console.log(`Producto con ID ${productIdBuscar} no encontrado.`);
+
+const zanahoria = {
+    title: "Zanahoria",
+    description: "vegetal naranja",
+    price: 100,
+    img: "imagen 1",
+    code: "COD01",
+    stock: 50
 }
 
 
-//Agregue la cantidad de productos necesarias para ejemplificar cada caso.
+manager.addProduct(zanahoria);
+
+
+const manzana = {
+    title: "Manzana",
+    description: "fruta roja y jugosa",
+    price: 250,
+    img: "imagen 2",
+    code: "COD02",
+    stock: 100
+}
+
+
+manager.addProduct(manzana);
+
+const banana = {
+    title: "Banana",
+    description: "fruta amarilla y nutritiva",
+    price: 350,
+    img: "imagen 3",
+    code: "COD03",
+    stock: 30
+}
+
+manager.addProduct(banana);
+
+const tomate = {
+    title: "Tomate",
+    description: "vegetal rojo",
+    price: 250,
+    img: "imagen 5",
+    code: "COD03",
+    stock: 230
+}
+
+manager.addProduct(tomate);
+
+const cebolla = {
+    title: "Cebolla",
+    description: "vegetal que al pelarlo puede hacerte llorar",
+    price: 450,
+    img: "imagen 6",
+    code: "COD05",
+    stock: 330
+}
+
+manager.addProduct(cebolla);
+
+manager.getproductos();
+
+
+async function busquedaPorId() {
+    const productoBuscado = await manager.getProductById(2);
+    console.log(productoBuscado);
+}
+
+busquedaPorId();
+
+async function eliminarProducto() {
+    await manager.deleteProduct(4); 
+    manager.getproductos();
+}
+
+const lechuga = {
+    id: 1,
+    title: "lechuga", 
+    description: "vegetal verde", 
+    price: 150,
+    img: "imagen 4",
+    code: "COD04",
+    stock: 130
+};
+
+async function funcionActualizar() {
+    await manager.updateProduct(1, lechuga);
+}
+
+async function iniciar() {
+    await eliminarProducto();
+    await funcionActualizar();
+}
+
+iniciar();
